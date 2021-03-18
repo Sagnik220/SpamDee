aa4fee6535ea2bea5e0a887d1c938abecf574e49
Sagnik Mukherjee <sagnik2207@gmail.com>
2021-03-19 00:02:34 +0530

    Update app.py

--- Desktop App/app.py
+++ Desktop App/app.py
@@ -8,7 +8,7 @@ pred = []
 #button callbak function
 #runs each time when the "Check" button is clicked
 def check_spam(pred):
-    with window("Message Spam Filter"):
+    with window("Spam Filter"):
         if pred == []:
             #runs only once - the the button is first clicked
             #and pred[-1] widget doesn't exist
@@ -28,23 +28,19 @@ def check_spam(pred):
         add_text(pred[-1], color=text_colour)
 
 #window object settings
-set_main_window_size(540, 720)
+set_main_window_size(720, 720)
 set_global_font_scale(1.25)
-set_theme("Gold")
+set_theme("Purple")
 set_style_window_padding(30,30)
 
-with window("Simple SMS Spam Filter", width=520, height=677):
+with window("Spam Filter", width=720, height=677):
     print("GUI is running...")
-    set_window_pos("Simple SMS Spam Filter", 0, 0)
-
-    #image logo
-    add_drawing("logo", width=520, height=290) #create some space for the image
+    set_window_pos("Spam Filter", 0, 0)
 
     add_separator()
     add_spacing(count=12)
     #text instructions
-    add_text("Please enter an SMS message of your choice to check if it's spam or not",
-    color=[232,163,33])
+    add_text("Enter a message of your choice to check if it's spam or not",color=[192,192,192])
     add_spacing(count=12)
     #collect input
     add_input_text("Input", width=415, default_value="type message here!")
@@ -52,11 +48,5 @@ with window("Simple SMS Spam Filter", width=520, height=677):
     #action button
     add_button("Check", callback=lambda x,y:check_spam(pred))
 
-#place the image inside the space
-draw_image("logo", "logo_spamFilter.png", [0, 240])
-
-#IF THE PREVIOUS LINE OF CODE TRIGGERS AN ERRROR TRY
-#draw_image("logo", "logo_spamFilter.png", [0,0], [458,192])
-
 start_dearpygui()
-print("Bye Bye, GUI")
+print("Bye, GUI")
